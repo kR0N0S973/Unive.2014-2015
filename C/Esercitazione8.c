fa367c751095582efa0f170f9f194641e0f96957
Kevin Wiede <kevinwiede@icloud.com>
2014-12-10 21:56:46 +0100

    added ex. strings

--- C/Esercitazione8.c
+++ C/Esercitazione8.c
@@ -1,6 +1,36 @@
 /* Copyright 2014-2015         *
  * coded by Kevin Wiederstein  */
 
+/* ####
+    STRINGS OF EX.8
+    "Choose the part to execute:\n\t1 \t: Part 1 (Bank Account)\n\t2 \t: Part 2 (Boat course)\n\t3 \t: Part 3 (Dungeon)\n\tother \t: Exit\n\n"
+    "Month %d balance is %f\n"
+    "Insert the random generator seed\n"
+    "Boat course:\n"
+    "(%d,%d)\n"
+    "Shallows:\n"
+    "(%d,%d)\n"
+    "The boat will run ashore at (%d,%d)\n"
+    "The boat course is valid\n"
+    "Insert the random generator seed\n"
+    "Player %d moving by %d tiles\n"
+    "Player %d is on a normal tile\n"
+    "Player %d is sliding by %d tiles\n"
+    "Player %d has defeated an enemy: Player rolled %d Enemy rolled %d\n"
+    "Player %d was defeated by an enemy: Player rolled %d Enemy rolled %d\n"
+    "Player %d has activated a trap and is going back to tile %d\n"
+    "Player %d has unclocked a door and is advancing to the next tile: Player rolled %d Lock level was %d\n"
+    "Player %d has not been able to open the lock and loses a turn\n"
+    "ERROR: invalid tile type\n"
+    "Player %d is at the dungeon entrance\n"
+    "Player %d has exited the dungeon\n"
+    "Player %d position: %d\n"
+    "Player %d current tile: %c\n"
+    "**********\n"
+    "Player %d has won\n"
+   ####
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
