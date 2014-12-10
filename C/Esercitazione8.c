6a87a601fe7fa24ac653897e7cac2312c4518411
Kevin Wiede <kevinwiede@icloud.com>
2014-12-10 21:19:14 +0100

    added esercitazione 8
    made the base for the excercise

---
+++ C/Esercitazione8.c
@@ -0,0 +1,11 @@
+/* Copyright 2014-2015         *
+ * coded by Kevin Wiederstein  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    printf("Choose the part to execute:\n\t1 \t: Part 1 (Bank Account)\n\t2 \t: Part 2 (Boat course)\n\t3 \t: Part 3 (Dungeon)\n\tother \t: Exit\n\n");
+
+}
