93b45f7636d217318d80e2db68bbcf301a56a506
Kevin Wiede <kevinwiede@icloud.com>
2014-12-11 13:02:40 +0100

    finished es2 and es3 of ex8
    not compiled yet

--- C/Esercitazione8.c
+++ C/Esercitazione8.c
@@ -1,39 +1,8 @@
 /* Copyright 2014-2015        *
  * coded by Kevin Wiederstein */
 
-/* ####
-	STRINGS OF EX.8
-	"Choose the part to execute:\n\t1 \t: Part 1 (Bank Account)\n\t2 \t: Part 2 (Boat course)\n\t3 \t: Part 3 (Dungeon)\n\tother \t: Exit\n\n"
-	"Month %d balance is %f\n"
-	"Insert the random generator seed\n"
-	"Boat course:\n"
-	"(%d,%d)\n"
-	"Shallows:\n"
-	"(%d,%d)\n"
-	"The boat will run ashore at (%d,%d)\n"
-	"The boat course is valid\n"
-	"Insert the random generator seed\n"
-	"Player %d moving by %d tiles\n"
-	"Player %d is on a normal tile\n"
-	"Player %d is sliding by %d tiles\n"
-	"Player %d has defeated an enemy: Player rolled %d Enemy rolled %d\n"
-	"Player %d was defeated by an enemy: Player rolled %d Enemy rolled %d\n"
-	"Player %d has activated a trap and is going back to tile %d\n"
-	"Player %d has unclocked a door and is advancing to the next tile: Player rolled %d Lock level was %d\n"
-	"Player %d has not been able to open the lock and loses a turn\n"
-	"ERROR: invalid tile type\n"
-	"Player %d is at the dungeon entrance\n"
-	"Player %d has exited the dungeon\n"
-	"Player %d position: %d\n"
-	"Player %d current tile: %c\n"
-	"**********\n"
-	"Player %d has won\n"
-	####
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 /* DICHIARAZIONE VARIABILI */
 	/* esercizio 1 */
@@ -43,19 +12,24 @@
 	int i;
 	/* esercizio 2 */
 	#define SHALLOW_DIM 4
+    #define COURSE_DIM  4
 	const int shallowsX[SHALLOW_DIM] = {3,1,2,0};
 	const int shallowsY[SHALLOW_DIM] = {0,1,3,4};
+    const int courseX[COURSE_DIM]    = {0,1,3,4};
+    int courseY[COURSE_DIM],randSeed,j,flag[1]={-1,-1};
 	/* esercizio 3 */
-	#define TILES_DIM 	10
-	#define EFFECTS_DIM 10
-	const char tiles[TILES_DIM]		 = {'n','s','n','s','e','t','n','n','e','d'};
-	const int effects[EFFECTS_DIM] 	 = {0,3,0,3,3,0,0,0,6,3};
+	#define DUNGEON_DIM 	10
+	const char tiles[DUNGEON_DIM]		 = {'n','s','n','s','e','t','n','n','e','d'};
+	const int effects[DUNGEON_DIM] 	     = {0,3,0,3,3,0,0,0,6,3};
+    int playerPosition[1]                = {-1,-1}
+    int player = 0;p1 = 0;p2 = 0;movRand = 0;Rand = 0;RandEnemy = 0;RandOpponent = 0;RandUnlock = 0;RandLock = 0;
 /* ####################### */ 
 
 void bankAccount();
 void boatCourse();
 void dungeon();
-
+void dungeon_do(char,int,int);
+    
 int main(){
 	int choose;
 	printf("Choose the part to execute:\n\t1 \t: Part 1 (Bank Account)\n\t2 \t: Part 2 (Boat course)\n\t3 \t: Part 3 (Dungeon)\n\tother \t: Exit\n\n");
@@ -73,7 +47,7 @@ int main(){
 
 /* (Bank Account) */
 void bankAccount(){
-	for(i=0;i<12;i++){
+	for(i=0;i<RATE_DIM;i++){
 		interesse = interesse + interestRates[i];
 		credito = credito + ((credito*interesse)/100);
 		printf("Month %d balance is %f\n",i,credito);
@@ -82,10 +56,72 @@ void bankAccount(){
 
 /* (Boat Course) */
 void boatCourse(){
-	
+	printf("Insert the random generator seed\n");
+    scanf("%d",&randSeed);
+    srand(randSeed);
+    printf("Boat course:\n");
+    for(j=0;j<COURSE_DIM;j++){
+        courseY[j] = 0 + rand() % 4;
+        if(shallowX[j] == courseX[j] && shallowY[j] == courseY[j]){flag[0]=shallowX[j];flag[1]=shallowy[j];}
+        printf("(%d,%d)\n",courseY[j],courseX[j]);}
+    printf("Shallows:\n");
+    for(j=0;j<COURSE_DIM;j++){printf("(%d,%d)\n",shallowY[j],shallowX[j]);}
+    if(flag[0] != -1 && flag[1] != -1){
+        printf("The boat will run ashore at (%d,%d)\n",flag[0],flag[1]);
+    }else{printf("The boat course is valid\n");}
 }
 
 /* (Dungeon) */
 void dungeon(){
-	
+    printf("Insert the random generator seed\n");
+    scanf("%d",&randSeed);
+    srand(randSeed);
+    /* start game */
+    while(!(playerPosition[0] <= 10 && playerPosition[1] <= 10)){
+        movRand = (1 + rand() % 3);
+        playerPosition[turn] = playerPosition[turn] + movRand;
+        printf("Player %d moving by %d tiles\n",turn,movRand);
+        if(playerPosition[turn] == 0){
+            printf("Player %d is at the dungeon entrance\n",turn);
+        }else if(playerPosition[turn] >= 10){
+            printf("Player %d has exited the dungeon\n",turn);
+        }else{
+            dungeon_do((tiles[playerPosition[turn]]),effects[playerPosition[turn]],turn);
+            printf("Player %d position: %d\n",turn,playerPosition[turn]);
+            printf("Player %d current tile: %c\n",turn,tiles[playerPosition[turn]]);
+        }
+        printf("**********\n");
+    }
+    printf("Player %d has won\n");
+}
+
+void dungeon_do(char c,int effect,int turn){
+    if(c=='n'){
+        printf("Player %d is on a normal tile\n",turn);
+    }else if(c=='s'){
+        Rand = -effect + rand() % effect;
+        printf("Player %d is sliding by %d tiles\n",Rand);
+        if(playerPosition[turn] + Rand)<-1){playerPosition[turn] = -1;}else{playerPosition[turn] = playerPosition[turn] + Rand;}
+    }else if(c=='e'){
+        RandOpponent = 1 + rand() % effect;
+        RandEnemy    = 1 + rand() % 6;
+        if(RandEnemy >= RandOpponent){
+            printf("Player %d has defeated an enemy: Player rolled %d Enemy rolled %d\n",turn,RandEnemy,RandOpponent);
+        }else{
+            printf("Player %d was defeated by an enemy: Player rolled %d Enemy rolled %d\n",turn,RandEnemy,RandOpponent);
+            playerPosition[turn] = -1;
+        }
+    }else if(c=='t'){
+        Rand = 0 + rand() % (playerPosition[turn]-1);
+        printf("Player %d has activated a trap and is going back to tile %d\n",turn,Rand);
+    }else if(c=='d'){
+        RandLock      = 1 + rand() % 6;
+        RandUnlock    = 1 + rand() % 6;
+        if(RandUnlock >= RandLock){
+            printf("Player %d has unclocked a door and is advancing to the next tile: Player rolled %d Lock level was %d\n",turn,RandUnlock,RandLock);
+            playerPosition[turn] = playerPosition[turn] + 1;
+        }else{
+            printf("Player %d has not been able to open the lock and loses a turn\n");
+        }
+    }else{printf("ERROR: invalid tile type\n");}
 }
